1fb48694f3377b9149146a60a23be03fcc79cf78
Md. Ahosan Habib <cseahosan@gmail.com>
2022-07-02 18:11:16 +0600

    add error handler

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express();
 app.use('/admin', adminRouter);
 app.use('/', publicRouter)
 
+// default error handler
+function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err);
+    }
+    res.status(500).json({ error: err });
+}
+
 app.listen(3000, () => {
     console.log('app listening on port 3000')
 })
